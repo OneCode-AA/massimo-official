1b783c6f284125e2095d4929a2bef75fbd4e9fb4
Mohamed Jalloh <onecode.aa@gmail.com>
2025-01-05 17:58:44 -0600

    new update - updated /archive and its css

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,20 +1,19 @@
 import { ArrowLeft, LinkOutlined } from '@mui/icons-material'
 import React from 'react'
 import Link from 'next/link'
+import '../app/globals.css';
 
 
-const archieves = [
+const archives = [
     {
       id: 1,
-      year: '',
-      name: '',
+      year: '2024',
+      name: 'todo-app',
       where: '',
-      linkurl: '',
+      linkurl: 'https://todo-app-self-kappa-88.vercel.app/',
       technologies: [
-      { id: 1, name: 'HTML & CSS' },
-      { id: 2, name: 'Javascript' },
-      { id: 3, name: 'React' },
-      { id: 4, name: 'Next.js' },
+      { id: 1, name: 'React' },
+      
     ],
   }, 
 ]
@@ -22,15 +21,13 @@ const archieves = [
 function Archive() {
   return (
     <section className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
-        
-        <Link href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</Link>
 
     <article className="lg:py-24">
     
-    <Link className="group mb-2 inline-flex items-center font-semibold leading-tight text-teal-300" href="/">
+    <Link  href="/" className="group mb-2 inline-flex justify-center items-center font-semibold leading-tight text-teal-300">
 
      <ArrowLeft/>
-    Brittany Chiang
+   Mohamed Jalloh
 
     </Link>
   
@@ -63,20 +60,24 @@ function Archive() {
     </tr>
     </thead>
     <tbody>
-    {archieves.map((archieve) => (
-    <tr className="border-b border-slate-300/10 last:border-none" key={archieve.id}>
+    {archives.map((archive) => (
+    <tr className="border-b border-slate-300/10 last:border-none" key={archive.id}>
     <td className="py-4 pr-4 align-top text-sm">
     <section className="translate-y-px">
-    {archieve.year}
+    {archive.year || "N/A"}
     </section>
     </td>
     <td className="py-4 pr-4 align-top font-semibold leading-snug text-slate-200">
     <section>
     <article className="block sm:hidden">
-    <Link className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 sm:hidden group/link text-base" href={`${archieve.linkurl}`}
-    target="_blank" rel="noreferrer noopener" aria-label="Emerson Collective (opens in a new tab)">
+    <Link className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 sm:hidden group/link text-base"
+     href={`${archive.linkurl}`}
+    target="_blank"
+    rel="noreferrer noopener"
+    aria-label={`${archive.name} (opens in a new tab)`}
+    >
     <span>
-    {archieve.name}
+    {archive.name}
     <span className="inline-block">
     <LinkOutlined/>
     </span>
@@ -84,20 +85,21 @@ function Archive() {
     </Link>
     </article>
     <article className="hidden sm:block">
-    {archieve.name}
+    {archive.name}
     </article>
     </section>
     </td>
     <td className="hidden py-4 pr-4 align-top text-sm lg:table-cell">
     <section className="translate-y-px whitespace-nowrap">
-    {archieve.where}
+    {archive.where || "N/A"}
     </section>
     </td>
     <td className="hidden py-4 pr-4 align-top lg:table-cell">
     <ul className="flex -translate-y-1.5 flex-wrap">
-    {archieve.technologies.map((tech) => (
-    <li className="my-1 mr-1.5" key={tech.name}>
+    {archive.technologies.map((tech) => (
+    <li className="my-1 mr-1.5" key={tech.id}>
     <section className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+    {tech.name}
  
     </section>
     </li>
@@ -106,10 +108,10 @@ function Archive() {
     </td>
     <td className="hidden py-4 align-top sm:table-cell"><ul className="translate-y-1">
         <li className="mb-1 flex items-center">
-            <Link className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-sm" href={`${archieve.linkurl}`} target="_blank" rel="noreferrer noopener" aria-label="emersoncollective.com (opens in a new tab)">
+            <Link className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-sm" href={`${archive.linkurl}`} target="_blank" rel="noreferrer noopener" aria-label="emersoncollective.com (opens in a new tab)">
             <span> 
                 <span className="inline-block">
-                    {archieve.name}
+                    {archive.name}
                    <LinkOutlined/>
                 </span>
                 </span>
